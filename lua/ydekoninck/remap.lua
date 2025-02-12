32c60eeea5f39cc675173ce0a33aff9d4c080ba2
Yannick De Koninck <ydekoninck@nvidia.com>
2025-02-12 09:25:28 +0100

    Adding remaps

--- lua/ydekoninck/remap.lua
+++ lua/ydekoninck/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
