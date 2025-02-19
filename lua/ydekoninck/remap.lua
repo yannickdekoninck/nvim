a18c6a6cac1c2c7601cb84fa4e3657eea79e996a
Yannick De Koninck <ydekoninck@nvidia.com>
2025-02-19 12:00:16 +0100

    Adding remap for diagnostics popup

--- lua/ydekoninck/remap.lua
+++ lua/ydekoninck/remap.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
 vim.keymap.set('n', '<Leader>d', ":Neogen<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>s', "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+
